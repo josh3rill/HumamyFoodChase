40c7230e602344b55d4ef63e43c1d2833c1eac4b
imogore amhanesi joshua <imogorej@gmail.com>
2024-03-24 08:53:45 +0100

    Z
    Z

--- HumamyFoodChase.php
+++ HumamyFoodChase.php
@@ -16,8 +16,8 @@ class FoodChase {
         // Calculate vertical movement based on direction
         $this->playerPos[1] += ($direction == 'down') ? 1 : (($direction == 'up') ? -1 : 0);
 
-        // Keep player within grid boundaries (left/right)
-//        $this->playerPos[0] = max(0, min($this->playerPos[0], $this->gridSize - 1));
+         Keep player within grid boundaries (left/right)
+        $this->playerPos[0] = max(0, min($this->playerPos[0], $this->gridSize - 1));
 
         // Keep player within grid boundaries (top/bottom)
 //        $this->playerPos[1] = max(0, min($this->playerPos[1], $this->gridSize - 1));
